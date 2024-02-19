02d991c1ea271e6919b4e19db450ebfc5e9a6b04
savanahagutu <savanahagutu0102@gmail.com>
2024-02-19 20:56:24 +0300

    added smooth effect using js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+//A smooth effect in the navbar links
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
 
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
